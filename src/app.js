b66a4fb15f8d5de56f635978b10cd1985282861c
pawanomprakash <141708273+pawanomprakash@users.noreply.github.com>
2023-12-11 15:51:48 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const foods = ["pizza", "burger", "fingerChips", "donuts", "springRoll"];
+const numberArray = [12,324,213,4,2,3,45,4234];
+const myArray = [11, 34, 20, 5, 53, 16];
+
+// Progression 1:
+function slice(start, end){
+  let modifiedFood = [];
+  for(let foodItem=start; foodItem<end-1; foodItem++){
+      modifiedFood.push(foods[foodItem]);
+  }
+  return modifiedFood;
+}
+
+
+// Progression 2:
+
+
+// Progression 3:
+
+
+// Progression 4:
+
+
+// Progression 5:
+
+
+// Progression 6:
+
+
+// Progression 7:
+
+
